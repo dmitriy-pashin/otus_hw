d57f7bb0ef7b550de4d9d8916cbf0cb3295c9c7f
Dmitriy Pashin <dmitriy.pashin@NB-PASHIN01.local>
2020-03-19 18:23:37 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	fmt.Printf("current time: %v\n", time.Now())
+
+	ntpTime, err := ntp.Time("time.apple.com")
+
+	if err != nil {
+		log.Print(err)
+		fmt.Println(err.Error())
+	}
+
+	fmt.Printf("exact time: %v\n", ntpTime)
 }
